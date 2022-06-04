90ed849f8156ba32f9c3f56e07ce15e38efee18f
oscarleonnogales <oscarleonnogales@gmail.com>
2022-06-04 01:35:05 -0500

    Setup Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home/Home';
 
 function App() {
 	return (
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Home.scss';
+
+type Props = {};
+
+export default function Home({}: Props) {
+	return <div className="title">Home</div>;
+}
