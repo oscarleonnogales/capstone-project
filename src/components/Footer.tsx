7b7bb92c77d1ac354d1a9bc9cdcb564da95b42c3
oscarleonnogales <oscarleonnogales@gmail.com>
2022-06-05 14:11:36 -0500

    File structure

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles/Footer.scss';
+
+type Props = {};
+
+export default function Footer({}: Props) {
+	return <div>Footer</div>;
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles/Header.scss';
+
+type Props = {};
+
+export default function Header({}: Props) {
+	return <div>Header</div>;
+}
