c042c7c21ec9d41087956a3ac9fe08d151c65e56
oscarleonnogales <oscarleonnogales@gmail.com>
2022-06-06 23:40:50 -0500

    Added placeholder files

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Slider from '../components/Slider';
+import ProductsGrid from '../components/ProductsGrid';
+import FeaturedProducts from '../components/FeaturedProducts';
 import './styles/Home.scss';
 
 export interface IHomeProps {}
@@ -9,6 +12,9 @@ const Home: React.FunctionComponent<IHomeProps> = (props) => {
 	return (
 		<>
 			<Header />
+			<Slider />
+			<ProductsGrid />
+			<FeaturedProducts />
 			<Footer />
 		</>
 	);
