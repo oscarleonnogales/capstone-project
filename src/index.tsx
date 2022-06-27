377294ca1828e31baccb0bc0ee4d97b12ecce686
oscarleonnogales <oscarleonnogales@gmail.com>
2022-06-27 18:19:41 -0500

    Persistent Store

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 import reportWebVitals from './reportWebVitals';
+
+const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<PersistGate loading={null} persistor={persistor}>
+				<App />
+			</PersistGate>
 		</Provider>
 	</React.StrictMode>
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,14 +3,28 @@ import productsReducer from './slices/productSlice';
 import bannersReducer from './slices/bannersSlice';
 import categoriesReducer from './slices/categoriesSlice';
 import filtersReducer from './slices/filtersSlice';
+import cartReducer from './slices/cartSlice';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
 
-export const store = configureStore({
-	reducer: {
-		products: productsReducer,
-		banners: bannersReducer,
-		categories: categoriesReducer,
-		filters: filtersReducer,
-	},
+const reducers = combineReducers({
+	products: productsReducer,
+	banners: bannersReducer,
+	categories: categoriesReducer,
+	filters: filtersReducer,
+	cart: cartReducer,
+});
+
+const persistConfig = {
+	key: 'root',
+	storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+	reducer: persistedReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
