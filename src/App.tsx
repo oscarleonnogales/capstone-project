40e9783de36164418add635e68de86ff2cf96e9e
oscarleonnogales <oscarleonnogales@gmail.com>
2022-06-27 21:57:48 -0500

    404 page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductListingsPage from './pages/ProductListingsPage';
-import AboutUsPage from './pages/AboutUsPage';
 import ProductDetailsPage from './pages/ProductDetailsPage';
 import CheckoutPage from './pages/CheckoutPage';
 import CartPage from './pages/CartPage';
 import SearchPage from './pages/SearchPage';
+import InvalidPage from './pages/InvalidPage';
 
 function App() {
 	return (
@@ -14,12 +14,12 @@ function App() {
 			<Routes>
 				<Route path="/" element={<HomePage />} />
 				<Route path="/home" element={<HomePage />} />
+				<Route path="/cart" element={<CartPage />} />
 				<Route path="/checkout" element={<CheckoutPage />} />
 				<Route path="/products" element={<ProductListingsPage />} />
 				<Route path="/product-details" element={<ProductDetailsPage />} />
-				<Route path="/aboutus" element={<AboutUsPage />} />
-				<Route path="/cart" element={<CartPage />} />
 				<Route path="/search" element={<SearchPage />} />
+				<Route path="*" element={<InvalidPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
